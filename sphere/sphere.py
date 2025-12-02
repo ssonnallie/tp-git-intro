d302404652c303dbf7df3dab81cee135611639ba
amondesir <amondesir@insa-lyon.fr>
2025-12-02 06:17:12 -0400

    add radius property

--- sphere/sphere.py
+++ sphere/sphere.py
@@ -3,9 +3,9 @@ import math, pickle
 class Sphere(object):
 
     def __init__(self, radius):
-        # *** STEP 1 ***
-        # uncomment the following line
-        #self.radius = radius
+         *** STEP 1 ***
+         uncomment the following line
+         self.radius = radius
         pass
 
     # *** STEP 2 ***
